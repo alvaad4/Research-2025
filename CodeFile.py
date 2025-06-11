cafa42652451cb47e0cbad60ade5b34be5494858
Adam Alvarado <adam@Adams-MBP.fu.campus>
2025-06-11 10:31:20 -0400

    Code

--- CodeFile.py
+++ CodeFile.py
@@ -1,2 +1,12 @@
-#Start code?
-#hello there
+import re 
+
+file = open('/Users/adam/Downloads/text.txt', 'r')
+content = file.read()
+
+pattern = r'\d+(:\.\d+)?'
+
+matches = re.findall(pattern,content)
+
+print(matches)
+
+file.close()
