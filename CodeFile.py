3e7dd2aad8fa4ea0c5694d59d9a25482a654d006
Adam Alvarado <adam@Adams-MBP.fu.campus>
2025-06-11 15:07:04 -0400

    Real Expression Code Work in Progress

--- CodeFile.py
+++ CodeFile.py
@@ -3,7 +3,7 @@ import re
 with open('/Users/adam/Downloads/text1.txt', 'r') as file:
     content = file.read()
 
-pattern = r'\d+(?::\.\d+)?'  # Adjust as needed
+pattern = r'(?:total duration:|prompt eval duration:|prompt eval rate:|eval duration:|evalrate:|prompt eval count:|eval count:|eval rate:|load duration:)\s*\d+(?:\.\d+)?'
 matches = re.findall(pattern, content)
 
 print(matches)
